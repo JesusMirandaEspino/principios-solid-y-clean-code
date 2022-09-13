31949ef52b7b7ba659d2a0b8b474013e48941b1d
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-13 14:54:01 -0500

    funciones if else

--- curso/src/clean-code/funciones.ts
+++ curso/src/clean-code/funciones.ts
@@ -42,6 +42,20 @@
 
     }
 
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
+
+        if ( isDead ) {
+            return 1500;
+        } 
+
+        if ( isSeparated ) {
+            return 2500;
+        } 
+        
+        return ( isRetired ) ? 3000 : 4000;
+        
+    }
     
 
 
