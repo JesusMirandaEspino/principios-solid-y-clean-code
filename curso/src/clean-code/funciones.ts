d393be024867b5b269d864cb770d62cbc37b9ee5
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-13 14:55:59 -0500

    funciones if else

--- curso/src/clean-code/funciones.ts
+++ curso/src/clean-code/funciones.ts
@@ -45,15 +45,13 @@
 
     const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
 
-        if ( isDead ) {
-            return 1500;
-        } 
+        if ( isDead ) return 1500;
 
-        if ( isSeparated ) {
-            return 2500;
-        } 
+        if ( isSeparated ) return 2500;
         
-        return ( isRetired ) ? 3000 : 4000;
+        if ( isRetired ) return 3000;
+        
+        return 4000;
         
     }
     
