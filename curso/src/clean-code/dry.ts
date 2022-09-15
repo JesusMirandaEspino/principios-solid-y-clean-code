b41950a62b25d9e35280c98692f0df34b8acf660
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-15 15:15:21 -0500

    dry

---
+++ curso/src/clean-code/dry.ts
@@ -0,0 +1,22 @@
+
+type Size = '' | 'S' | 'M' | 'XL';
+
+class Product {
+    constructor(    public name: string = '',
+                    public price: number = 0,
+                    public size: Size = ''){
+    }
+
+    toString(){
+        if(this.name.length < 0) throw new Error('Name is empty.');
+        if(this.price  <= 0) throw new Error('Price is wrong.');
+        if(this.size.length <=  0) throw new Error('Size is empty.');
+
+        return `${this.name} (${this.price}), ${this.size}`
+    }
+}
+
+(() => {
+    const bluePants = new Product('Blue pants');
+    console.log(bluePants);
+})();
