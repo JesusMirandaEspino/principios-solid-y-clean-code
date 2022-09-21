cc3db4338ef94636e02455b6c926100fc0558076
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-09-20 19:37:23 -0500

    classes herencia

--- curso/src/clean-code/classes.ts
+++ curso/src/clean-code/classes.ts
@@ -3,7 +3,6 @@
     type Gender = 'M' | 'F';
 
     class Person {
-
         constructor(    
                 public name: string, 
                 public gender: Gender, 
@@ -17,22 +16,48 @@
     }
 
     class User extends Person {
+        private lastAccess: Date
         constructor(
             public email: string,
             public role: string,
-            private lastAccess: Date,
             name: string,
             gender: Gender,
             birthDate: Date
         ){
             super( name, gender, birthDate);
+            this.lastAccess = new Date;
         }
 
         checkCredentials(){
             return true;
         }
-
-        
     }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory:    string,
+            public lastOpenFolder:      string,
+            email:      string,
+            role:       string,
+            name:       string,
+            gender:     Gender,
+            birthDate:  Date
+
+        ){
+            super( email, role, name, gender, birthDate );
+        }
+    }
+
+    const userSettings = new UserSettings(
+        'usr/home',
+        'home',
+        'jesus.supercorreo@gmail.com',
+        'admin',
+        'Jesus',
+        'M',
+        new Date('1988-04-16')
+    );
+
+    console.log({userSettings})
 
 })();
