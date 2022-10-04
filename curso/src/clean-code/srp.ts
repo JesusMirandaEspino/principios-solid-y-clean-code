a0836169be514d75cf988624d0aa08550929f6ac
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-04 17:41:31 -0500

    srp part 4

--- curso/src/clean-code/srp.ts
+++ curso/src/clean-code/srp.ts
@@ -29,15 +29,23 @@
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
     class ProductBloc {
+
+        private productServices: ProductServices;
+        private mailer: Mailer;
+
+        constructor(  productServices: ProductServices,  mailer: Mailer ){
+            this.productServices = productServices;
+            this.mailer = mailer;
+        }
     
         loadProduct( id: number ) {
             // Realiza un proceso para obtener el producto y retornarlo
-            console.log('Producto: ',{ id, name: 'OLED Tv' });
+            this.productServices.getProduct(id);
         }
     
         saveProduct( product: Product ) {
             // Realiza una petición para salvar en base de datos 
-            console.log('Guardando en base de datos', product );
+            this.productServices.saveProduct(product);
         }
     
         notifyClients() {
@@ -60,8 +68,10 @@ class CartBlog{
   
     }
 
+    const productServices = new ProductServices();
+    const mailer = new Mailer();
 
-    const productBloc = new ProductBloc();
+    const productBloc = new ProductBloc(productServices, mailer);
     const cartBlog = new CartBlog();
 
     productBloc.loadProduct(10);
