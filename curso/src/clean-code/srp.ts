0dfa11b2420ffac7ea356c4f3b025c8753e1d9d0
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-04 17:24:37 -0500

    srp part 2

--- curso/src/clean-code/srp.ts
+++ curso/src/clean-code/srp.ts
@@ -4,6 +4,14 @@
         id:   number;
         name: string;
     }
+
+
+    class ProductServices {
+        getProduct( id: number ) {
+            // Realiza un proceso para obtener el producto y retornarlo
+            console.log('Producto: ',{ id, name: 'OLED Tv' });
+        }
+    }
     
     // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
     // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
@@ -22,22 +30,31 @@
         notifyClients() {
             console.log('Enviando correo a los clientes');
         }
-    
-        onAddToCart( productId: number ) {
-            // Agregar al carrito de compras
-            console.log('Agregando al carrito ', productId );
-        }
+
     
     }
     
 
 
+class CartBlog{
+
+        private itemsInCart: Object[] = [];
+
+        addToCart( productId: number ) {
+            // Agregar al carrito de compras
+            console.log('Agregando al carrito ', productId );
+        }
+  
+    }
+
+
     const productBloc = new ProductBloc();
+    const cartBlog = new CartBlog();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
     productBloc.notifyClients();
-    productBloc.onAddToCart(10);
+    cartBlog.addToCart(10);
 
 
 
