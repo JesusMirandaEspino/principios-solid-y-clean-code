ccd7b50a7dd22e51acb4444f5be0b89c58be62ed
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-10-11 18:06:54 -0500

    segregacion part 1

---
+++ curso/src/clean-code/segregacion.ts
@@ -0,0 +1,29 @@
+interface Bird{
+    fly(): void;
+    eat(): void;
+    run(): void;
+    swin(): void;
+}
+
+
+
+class Tucan implements Bird{
+    public fly(){}
+    public eat(){}
+    public run(){}
+    public swin(){}
+}
+
+class Carpintero implements Bird{
+    public fly(){}
+    public eat(){}
+    public run(){}
+    public swin(){}
+}
+
+class Pinguino implements Bird{
+    public fly(){}
+    public eat(){}
+    public run(){}
+    public swin(){}
+}
